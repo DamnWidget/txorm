ee90bc19b85afa3dbe84506789b53dfef25c454b
Oscar Campos <oscar.campos@member.fsf.org>
2014-06-07 22:37:32 +0100

    _object_data.c prototype, C extension is broken right now, 15 tests are not passing when C extension is enabled

---
+++ txorm/_object_data.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2014 Oscar Campos <oscar.campos@member.fsf.org>
+ * See LICENSE for details
+ */
+
+#include "include/txorm.h"
+
+#if PY_MAJOR_VERSION >= 3
+    #define _PY3
+#endif
