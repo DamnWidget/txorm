0d92b543421c62c9f4150332a771f54e7970ebec
Oscar Campos <oscar.campos@dedsert.com>
2014-08-22 16:14:07 +0100

    connection object, work in progress

--- txorm/database/connection.py
+++ txorm/database/connection.py
@@ -5,7 +5,7 @@
 from __future__ import unicode_literals
 
 from twisted.python import log
-from twisted.internet import defer
+from twisted.internet import defer, threads
 
 from txorm.compiler.state import State
 from txorm.signal import signal, Signal
@@ -14,7 +14,7 @@ from txorm.compiler import txorm_compile
 from txorm.compiler.expressions import Expression
 
 
-class ConnectionPool(object):
+class Connection(object):
     """A connection pool to a database
     """
 
@@ -27,11 +27,16 @@ class ConnectionPool(object):
         self.register_transaction = Signal(self)
 
     @defer.inlineCallbacks
-    @signal('register_transaction')
     def execute(self, statement, params=None, noresult=False, **kwargs):
         """
         Execute a statement with the given parameters and return a defer
-        object which will fire the return value or a Failure
+        object which will fire the return value or a Failure.
+
+        .. note::
+
+            This method will automatically commit, so don't use it if
+            you need transactional behavior, in that case, use
+            :method:`Connection.execute_transact`
 
         :param statement: the statement or expression to execute
         :type statement: :class:`Expression` or string
@@ -45,10 +50,61 @@ class ConnectionPool(object):
             if params is not None:
                 raise ValueError('Can\'t pass parameters with expressions')
             state = State()
-            result = yield self._raw_connection.runQuery()
             statement = self.compile(statement, state)
             if noresult is False:
                 result = yield self._execute(statement, *params, **kwargs)
                 defer.returnValue(self.result_factory(result))
             else:
-                self._raw_connection.runOperation(statement, *params, **kwargs)
+                self._raw_connection.runOperation(
+                    *self._execution_args(params, statement), **kwargs)
+
+    @defer.inlineCallbacks
+    def execute_transact(self, transact_chain, *args, **kwargs):
+        """
+        Execute a transaction calling the transact_chain that defines
+        what operations are going to be performed into the database.
+
+        This method commits at the end or rollback if there is some
+        failure.
+
+        If you call this from user code, you have to make sure to make:
+
+            state = State()
+            txorm_compile(statement, state)
+
+        in some moment to compile your expressions
+
+        :param transact_chain: a callable object whose first argument
+            is a :class:`twisted.adbapi.Transaction`
+        :type transact_chain: callable
+        :param args: additional positional arguments to be passed to
+            the transaction
+        :type args: list
+        :param kwargs: keyword arguments to be passed to the interaction
+        :type kwargs: dict
+        :return: a Deferred which will fire the return value of
+            `transact_chain(Transaction(...), *args, **kwargs)` or
+            Failure
+        """
+
+        return self._raw_connection.rinInteraction(
+            transact_chain, *args, **kwargs
+        )
+
+    def _execute(self, statement, *params, **kwargs):
+        """Execute raw statement using twisted adbapi
+        """
+
+        args = self._execution_args(params, statement)
+        return self._raw_connection.runQuery(*args, **kwargs)
+
+    def _execution_args(self, params, statement):
+        """Get the appropiate statement execution arguments
+        """
+
+        if params:
+            args = (statement, tuple(self.to_database(params)))
+        else:
+            args = (statement, )
+
+        return args
